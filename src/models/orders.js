4b5d0a2072d1761b2bb7bf00c5a9360f1414422f
Maxi Fernandez <maxi_fernandez2012@hotmail.com.ar>
2025-11-12 19:15:03 -0300

    order modificado

--- src/models/orders.js
+++ src/models/orders.js
@@ -2,50 +2,16 @@ import mongoose from "mongoose";
 
 const orderModel = new mongoose.Schema(
   {
-    usuario: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "users",
-      required: true
-    },
-
-    fecha: {
-      type: Date,
-      default: Date.now
-    },
-
-    metodoPago: {
-      type: String,
-      enum: ["Transferencia", "Efectivo", "Tarjeta"],
-      required: true
-    },
-
-    estado: {
-      type: String,
-      enum: ["Pendiente", "En preparación", "Enviado", "Entregado", "Cancelado"],
-      default: "Pendiente"
-    },
-
-    total: {
-      type: Number,
-      required: true
-    },
-
+    usuario: {type: mongoose.Schema.Types.ObjectId,nref: "users", required: true},
+    fecha: {type: Date, default: Date.now},
+    metodoPago: {type: String, enum: ["Transferencia", "Efectivo", "Tarjeta"], required: true},
+    estado: {type: String,enum: ["Pendiente", "En preparación", "Enviado", "Entregado", "Cancelado"],default: "Pendiente"},
+    total: {type: Number,required: true},
     items: [
       {
-        producto: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "products",
-          required: true
-        },
-        cantidad: {
-          type: Number,
-          required: true,
-          min: 1
-        },
-        subtotal: {
-          type: Number,
-          required: true
-        }
+        producto: {type: mongoose.Schema.Types.ObjectId,ref: "products",required: true},
+        cantidad: {type: Number,required: true,min: 1},
+        subtotal: {type: Number,required: true}
       }
     ]
   },
